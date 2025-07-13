18730343ead99ca0940580311f8341f720045da0
IsmailAbbasi <ismailabbasi118@gmail.com>
2025-07-13 12:29:57 +0530

    I have updated daatabse from sqlite to Mongo DB

--- MedsApp/settings.py
+++ MedsApp/settings.py
@@ -74,12 +74,17 @@ WSGI_APPLICATION = 'MedsApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3', 
+#     }
+# }
+try:
+    from .settings_local import *
+except ImportError:
+    pass
 
 
 # Password validation
