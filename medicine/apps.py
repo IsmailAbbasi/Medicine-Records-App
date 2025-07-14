1e4ca9c2c3dc12b952e1e8dbc2c9fd5e231a6ed3
IsmailAbbasi <ismailabbasi118@gmail.com>
2025-07-14 21:22:41 +0530

    Set random username for all users logging in

--- medicine/apps.py
+++ medicine/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
-
+from django.core.signals import request_finished
+from . import signals
 
 class MedicineConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'medicine'
+
+    def ready(self):
+        request_finished.connect(signals.random_username)
